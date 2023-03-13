efddbc6313e06727603b8fd0ee89f69c4191ae45
Pavel S <suslov9876@gmail.com>
2023-03-14 02:34:41 +0300

    First static dump version

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 CC = clang
 LLC = llc
 OPT = opt
+DOT = dot
 
 PASSDIR = ~/Git/llvm-pass/pass/
 PASSNAME = libpass.so
@@ -8,25 +9,34 @@ PASSNAME = libpass.so
 CXXFLAGS  = -c -O0 -emit-llvm -Wall -Wextra
 LDFLAGS = 
 OPTFLAGS = -enable-new-pm=0
+DOTFLAGS = -Tpng
 
 SRCDIRS = ./src/
 TARGETDIRS = ./targets/
 BINDIR = ./bin/
 INCLUDEDIR = ./include/
+GRAPHDIR = ./graphs/
+DUMPDIR = ./dumps/
 
 SOURCES = $(wildcard $(SRCDIRS)*.c)
 TARGETS = $(wildcard $(TARGETDIRS)*.c)
+GRAPHS = $(wildcard $(GRAPHDIR)*.txt)
 
 OBJECTS = $(addprefix $(BINDIR), $(notdir $(SOURCES:.c=.o)))
 TARGETOBJECTS = $(addprefix $(BINDIR), $(notdir $(TARGETS:.c=.o)))
+DUMPS = $(addprefix $(DUMPDIR), $(notdir $(GRAPHS:.txt=.png)))
 
 DEPENDENCES = $(addsuffix .d,$(OBJECTS))
 TARGETDEPENDENCES = $(addsuffix .d,$(TARGETOBJECTS))
 
 TARGETSNOEXT =  $(notdir $(TARGETS:.c=))
+DUMPSNOEXT = $(notdir $(GRAPHS:.txt=))
 
 $(TARGETSNOEXT): $(OBJECTS) $(TARGETOBJECTS)
 	@$(CC) $(LDFLAGS) $(OBJECTS) $(BINDIR)$@.o -o $@.out
+
+$(DUMPDIR)%.png: $(GRAPHDIR)%.txt
+	@$(DOT) $(DOTFLAGS) $< > $@
 
 $(BINDIR)%.o: $(SRCDIRS)%.c $(PASSDIR)$(PASSNAME)
 	@$(CC) -MMD -MF $@.d $(CXXFLAGS) $< -o $@.bc
@@ -44,9 +54,12 @@ $(PASSDIR)$(PASSNAME): $(PASSDIR)MyPass.cpp
 -include $(DEPENDENCES)
 -include $(TARGETDEPENDENCES)
 
-.PHONY: all, clean, mkdirs, pass
+.PHONY: all, clean, mkdirs, dumps
 
 all: Library
+
+dumps: $(DUMPS)
+	@echo "Dumps created"
 
 mkdirs:
 	mkdir $(BINDIR) $(SRCDIRS) $(TARGETDIRS) $(INCLUDEDIR)
--- pass/MyPass.cpp
+++ pass/MyPass.cpp
@@ -7,6 +7,8 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 
+const char graph_path[] = "./graphs/static.txt"; 
+
 using namespace llvm;
 
 namespace {
@@ -14,16 +16,69 @@ namespace {
     // Define your Pass as a subclass of one of the existing pass interfaces.
     struct MyPass : public FunctionPass {
         static char ID;
-        MyPass() : FunctionPass(ID) {}
+        MyPass() : FunctionPass(ID), output_(nullptr), depth_(0) {
+            output_ = fopen(graph_path, "w");
+            assert(output_);
+
+            PrintNTabs();
+            fprintf(output_, "digraph {\n");
+            depth_++;
+            
+            PrintNTabs();
+            fprintf(output_, "rankdir=HR; outputOrder=nodesfirst;\n");
+        }
+
+        ~MyPass() {
+            depth_--;
+            PrintNTabs();
+            fprintf(output_, "}\n");
+            
+            fclose(output_);
+        }
 
         // This function is provided by FunctionPass. It runs on every function
         // in a LLVM IR module and passes a reference to the current function
         // as parameter
         bool runOnFunction(Function &Func) override {
-            outs() << "Running on function " << Func.getName() << "\n";
+            PrintNTabs();
+            fprintf(output_, "subgraph %s {\n", Func.getName());
             
+            PrintNTabs();
+            fprintf(output_, "label = \"%s\"\n", Func.getName());
+            depth_++;
+
+            for (auto& B : Func) {
+                for (auto& I : B) {
+                    PrintNTabs();
+                    fprintf(output_, "\t%lu[label = \"%s\"];\n", &I, I.getOpcodeName());
+                    depth_++;
+
+                    for (auto* user : I.users()) {
+                        PrintNTabs();
+                        fprintf(output_, "\t\t%lu -> %lu\n", &I, user);
+                    }
+
+                    depth_--;
+                }
+            }
+
+            depth_--;
+            PrintNTabs();
+            fprintf(output_, "}\n");
+
             return true;
-        }
+        }  
+
+        private:
+            void PrintNTabs() {
+                uint64_t depth = depth_;
+                while (depth--) {
+                    fputc('\t', output_);
+                }
+            }
+
+            FILE* output_;
+            uint64_t depth_;
     };
 }
 
